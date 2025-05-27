3a6c36c5db44f3be22662275e8c8ce4dcfd94ac3
SaudBasra <117218133+SaudBasra@users.noreply.github.com>
2025-05-27 23:10:00 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'local-default-secret-key')
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
 # ALLOWED_HOSTS handles both development and production
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,pulseprep.net, 127.0.0.1').split()
 
 # Application definition
 INSTALLED_APPS = [
